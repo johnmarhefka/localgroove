8f4afa81aca3c2b3fbd45c2c7fdfb39011c7f866
John Marhefka <johnmarhefka@Johns-MacBook-Pro.local>
2017-02-09 16:41:15 -0500

    Began adding venmo tipping logic.

--- src/pages/tip/tip.ts
+++ src/pages/tip/tip.ts
@@ -2,6 +2,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { InAppBrowser, InAppBrowserEvent } from 'ionic-native';
+import { AppAvailability, Device } from 'ionic-native';
 
 @Component({
   selector: 'page-tip',
@@ -20,4 +22,33 @@ export class TipPage {
   ngOnInit(): void {
   }
 
+  // Opens venmo to tip the desired person.
+  // TODO: make it actually tip the person you're looking at
+  tipTapped(event) {
+    let app;
+
+    debugger;
+
+    if (Device.device.platform === 'iOS') {
+      app = 'venmo://';
+    } else if (Device.device.platform === 'Android') {
+      app = 'com.venmo';
+    }
+
+    AppAvailability.check(app)
+      .then(
+      function (response) {
+        debugger;
+        // This seems to only be reached when the app is there.
+        let browser = new InAppBrowser('venmo://paycharge?txn=pay&audience=private&recipients=venmo@venmo.com&amount=1&note=You%20guys%20rock', '_system');
+      }
+      )
+      .catch(function (e) {
+        debugger;
+        // This is (for now) our way of telling if the app isn't there and deciding to launch in a browser.
+        let browser = new InAppBrowser('https://venmo.com/?txn=pay&audience=private&recipients=venmo@venmo.com&amount=1&note=You%20guys%20rock!', '_system');
+      });
+
+    
+  }
 }
