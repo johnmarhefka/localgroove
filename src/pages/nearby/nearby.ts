c42179da57b8a38f9cf653d829efe4a8d036b352
John Marhefka <johnmarhefka@Johns-MacBook-Pro.local>
2017-04-01 12:35:14 -0400

    Use actual location of device for nearby venues

--- src/pages/nearby/nearby.ts
+++ src/pages/nearby/nearby.ts
@@ -1,11 +1,11 @@
 
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
 
 import { VenueService } from '../../services/venue.service';
 import { VenueDetailsPage } from './../venue-details/venue-details';
-//TODO: need this back when we go back to getting an actual location
-//import { Geolocation, Geoposition } from 'ionic-native';
+
 
 @Component({
   selector: 'page-nearby',
@@ -19,15 +19,15 @@ export class NearbyPage {
 
   ngOnInit(): void {
 
-    // TODO: This is fake code to get locations in Fells to avoid latency. Is getCurrentPosition() only slow on a laptop?
-    this.getVenues(39.2821, -76.5916);
+    // This is fake code to get locations in Fells to avoid latency.
+    //this.getVenues(39.2821, -76.5916);
 
     // // Get current location and pass it along to a function that calls the VenueService.
-    // Geolocation.getCurrentPosition().then((resp) => {
-    //   this.getVenues(resp.coords.latitude, resp.coords.longitude);
-    // }).catch((error) => {
-    //   console.log('Error getting location', error);
-    // });
+    Geolocation.getCurrentPosition().then((resp) => {
+      this.getVenues(resp.coords.latitude, resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
   getVenues(latitude: number, longitude: number): void {
