f90a617e9e2a79db1569febee4a3e8433718995a
John Marhefka <johnmarhefka@Johns-MacBook-Pro.local>
2017-04-17 20:26:33 -0400

    Loading icon on welcome page.

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { InAppBrowser } from 'ionic-native';
 
 import { TabsPage } from './../tabs/tabs';
@@ -15,21 +15,32 @@ export class WelcomePage {
   paymentAppButtonHidden: boolean = true;
   paymentAppConfirmationHidden: boolean = true;
 
-  constructor(public navCtrl: NavController, private paymentService: PaymentService) {
-    // Check if they have the payment app installed.
+  constructor(public navCtrl: NavController, private paymentService: PaymentService, public loading: LoadingController) { }
+
+  ngOnInit(): void {
+    let loader = this.loading.create();
+    loader.present().then(() => {
+      this.checkAppAvailability(loader);
+    });
+  }
+
+  // Checks if they have the payment app installed.
+  checkAppAvailability(loader?): void {
     this.paymentService.getAppAvailability().then(
       (positiveResponse) => { // Success callback (they have the payment app)
         this.paymentAppButtonHidden = true;
         this.paymentAppConfirmationHidden = false;
+        if (loader)
+          loader.dismiss();
       },
       (negativeResponse) => { // Error callback
         this.paymentAppButtonHidden = false;
         this.paymentAppConfirmationHidden = true;
+        if (loader)
+          loader.dismiss();
       }
     );
   }
-
-  ngOnInit(): void { }
 
   getVenmoTapped(event) {
     new InAppBrowser(this.paymentService.getAppDownloadUrl(), '_system')
